6999d7702873efb6e7dba60c0524641f5e4a00cf
Sławomir Kwasiborski <Slawomir.Kwasiborski@internetium.pl>
2021-09-25 16:29:57 +0200

    Reverts the change to the stored deviceId

--- index.js
+++ index.js
@@ -29,8 +29,7 @@ function MiFlowerCarePlugin(log, config) {
     this.log = log;
     this.name = config.name;
     this.displayName = this.name;
-    // normalize the deviceId to the same form as reported by the device.
-    this.deviceId = config.deviceId.toLowerCase().replace(/:/g, '');
+    this.deviceId = config.deviceId;
     this.interval = Math.min(Math.max(config.interval, 1), 600);
 
     this.config = config;
@@ -58,7 +57,7 @@ function MiFlowerCarePlugin(log, config) {
     this.flora = new MiFlora(this.deviceId);
 
     this.flora.on('data', function (data) {
-        if (data.deviceId == that.deviceId) {
+        if (data.deviceId == that.deviceId.toLowerCase().replace(/:/g, '')) {
             that.log("Lux: %s, Temperature: %s, Moisture: %s, Fertility: %s", data.lux, data.temperature, data.moisture, data.fertility);
             that.storedData.data = data;
             
@@ -105,7 +104,7 @@ function MiFlowerCarePlugin(log, config) {
     });
 
     this.flora.on('firmware', function (data) {
-        if (data.deviceId == that.deviceId) {
+        if (data.deviceId == that.deviceId.toLowerCase().replace(/:/g, '')) {
             that.log("Firmware: %s, Battery level: %s", data.firmwareVersion, data.batteryLevel);
             that.storedData.firmware = data;
 
